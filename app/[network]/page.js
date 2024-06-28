2bc0ba9c02b9be9094eedf978415154a748e6a75
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2024-06-28 09:26:02 +0530

    chore: faucet page

---
+++ app/[network]/page.js
@@ -0,0 +1,12 @@
+import FaucetInfo from "@/components/ui/FaucetInfo"
+import NetworkInfo from "@/components/ui/NetworkInfo"
+
+export default function Page({ params }) {
+  console.log(params)
+  return (
+    <div className="flex flex-col items-center ">
+      <NetworkInfo network={params.network} />
+      <FaucetInfo network={params.network} />
+    </div>
+  )
+}
