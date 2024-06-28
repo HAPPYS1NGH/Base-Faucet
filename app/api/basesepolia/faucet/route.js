814b7661148f4d44904f17c377224a5aa0b43427
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2024-06-29 01:00:34 +0530

    feat: base api

---
+++ app/api/basesepolia/faucet/route.js
@@ -0,0 +1,104 @@
+import { baseSepoliaClient } from "@/lib/client";
+import { faucetInfo } from "@/constants";
+import { getLastTransactionTimestampForAddress } from "@/lib/utils/alchemySDK";
+
+// Utility function to introduce a delay
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+// Function to process a single address
+const processAddress = async (address) => {
+  try {
+    const bal = await baseSepoliaClient.getBalance({ address: address });
+
+    // Convert Big Number and divide by 10^18 to get the balance in ETH and convert
+    // the balance to 4 decimal places
+    const balance =
+      bal.toString().slice(0, -18) +
+      "." +
+      bal.toString().slice(-18).slice(0, 4);
+    console.log("HIHIHIIHHIHHIH bal", balance);
+
+    const lastActive = await getLastTransactionTimestampForAddress(
+      address,
+      "base-sepolia"
+    );
+
+    console.log("lastActive after data.result in ARB", lastActive);
+    // Convert the time elapsed between the last active time and the current time in UTC accordingly in minutes, hours, days, weeks, months or years
+    const timeElapsed = Date.now() / 1000 - lastActive;
+    console.log("timeElapsed", timeElapsed);
+    let time = "";
+
+    // Determine the appropriate unit based on the time elapsed
+    let unit = "";
+    if (timeElapsed < 60) {
+      unit = timeElapsed === 1 ? "second" : "seconds";
+      time = `${Math.floor(timeElapsed)} ${unit} ago`;
+    } else if (timeElapsed < 3600) {
+      unit = Math.floor(timeElapsed / 60) === 1 ? "minute" : "minutes";
+      time = `${Math.floor(timeElapsed / 60)} ${unit} ago`;
+    } else if (timeElapsed < 86400) {
+      unit = Math.floor(timeElapsed / 3600) === 1 ? "hour" : "hours";
+      time = `${Math.floor(timeElapsed / 3600)} ${unit} ago`;
+    } else if (timeElapsed < 604800) {
+      unit = Math.floor(timeElapsed / 86400) === 1 ? "day" : "days";
+      time = `${Math.floor(timeElapsed / 86400)} ${unit} ago`;
+    } else if (timeElapsed < 2628000) {
+      unit = Math.floor(timeElapsed / 604800) === 1 ? "week" : "weeks";
+      time = `${Math.floor(timeElapsed / 604800)} ${unit} ago`;
+    } else if (timeElapsed < 31536000) {
+      unit = Math.floor(timeElapsed / 2628000) === 1 ? "month" : "months";
+      time = `${Math.floor(timeElapsed / 2628000)} ${unit} ago`;
+    } else {
+      unit = Math.floor(timeElapsed / 31536000) !== 1 ? "years" : "year";
+      time = `${
+        timeElapsed
+          ? `${Math.floor(timeElapsed / 31536000)} ${unit} ago`
+          : "1 year ago"
+      } `;
+    }
+
+    return {
+      balance: balance,
+      lastActive: time,
+      timestamp: lastActive,
+      address: address,
+    };
+  } catch (e) {
+    return {
+      balance: "0",
+      lastActive: "0",
+      timestamp: 0,
+      address: address,
+    };
+  }
+};
+
+export async function GET() {
+  try {
+    const faucets = faucetInfo.BaseSepolia;
+    console.log("faucets", faucets);
+    const addresses = faucets.map((faucet) => faucet.address);
+    console.log("addresses", addresses);
+
+    const batchSize = 4; // Number of addresses to process per batch
+    const delayMs = 1000; // Delay between batches in milliseconds
+
+    let faucetData = [];
+
+    for (let i = 0; i < addresses.length; i += batchSize) {
+      const batch = addresses.slice(i, i + batchSize);
+      const batchResults = await Promise.all(batch.map(processAddress));
+      faucetData = faucetData.concat(batchResults);
+      if (i + batchSize < addresses.length) {
+        await delay(delayMs); // Wait before processing the next batch
+      }
+    }
+
+    console.log("faucetData", faucetData);
+    return new Response(JSON.stringify({ faucetData }), { status: 200 });
+  } catch (e) {
+    console.log(e);
+    return new Response(JSON.stringify({ error: "Error" }), { status: 500 });
+  }
+}
---
+++ app/api/basesepolia/route.js
@@ -0,0 +1,25 @@
+import { baseSepoliaClient } from "@/lib/client";
+
+export async function GET() {
+  let blockNumber = await baseSepoliaClient.getBlockNumber();
+  let gasPrice = await baseSepoliaClient.getGasPrice();
+  blockNumber = blockNumber.toString();
+  gasPrice = gasPrice.toString();
+  //  Convert gas price into wei, gwei whichever is correct for 2 decimal place use K for 1000 and million M
+  let gasPriceUnit;
+  if (parseFloat(gasPrice) > 1e9) {
+    gasPriceUnit = "Gwei"; // Keep in Gwei if above 1 billion
+    gasPrice = parseFloat(gasPrice).toFixed(2); // Ensure two decimal places
+  } else if (parseFloat(gasPrice) >= 1e6) {
+    gasPriceUnit = "K Wei"; // Convert to kilo-Gwei (k) if between 1 million and 1 billion
+    gasPrice = (parseFloat(gasPrice) / 1e6).toFixed(2); // Divide by 1e6 and ensure two decimal places
+  } else {
+    gasPriceUnit = "M Wei"; // Convert to mega-Gwei (M) if below 1 million
+    gasPrice = (parseFloat(gasPrice) / 1e6).toFixed(2); // Divide by 1e6 and ensure two decimal places
+  }
+  let gasValue = `${gasPrice}${gasPriceUnit}`;
+  console.log(gasValue);
+  console.log("blockNumber", blockNumber);
+  console.log("gasPrice", gasPrice);
+  return Response.json({ blockNumber, gasPrice: gasValue });
+}
